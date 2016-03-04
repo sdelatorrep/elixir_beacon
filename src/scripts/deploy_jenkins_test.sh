432400c65116867fadd5f6c00c68eb1b7e4dd22e
sdelatorrep <sabela.delatorre@crg.eu>
2016-03-04 09:17:44 +0100

    Not needed file.

--- src/scripts/deploy_jenkins_test.sh
+++
@@ -1,5 +0,0 @@
-mv "${WORKSPACE}/target/elixir-beacon-0.0.1-SNAPSHOT.jar" "${WORKSPACE}/target/elixirbeacon-service.jar"
-scp "${WORKSPACE}/target/elixirbeacon-service.jar"  omartinez@apptest01:
-ssh omartinez@apptest01 "sudo bash -c '/etc/init.d/elixirbeaconservice_test stop && sleep 30'"
-ssh omartinez@apptest01 "sudo bash -c 'cp /home/omartinez/elixirbeacon-service.jar /microservices/elixirbeaconservice/test/ && chown -R elixirbeaconservice.elixirbeaconservice /microservices/elixirbeaconservice/test/'"
-ssh omartinez@apptest01 "sudo bash -c 'sleep 30 && cd /microservices/elixirbeaconservice/test/ && nohup /etc/init.d/elixirbeaconservice_test start > /dev/null 2>&1 &'"
